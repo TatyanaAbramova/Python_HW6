4ed028d0f38e04507a18fe47775acacab6b27fee
Tatyana Abramova <tanusha-atv@mail.ru>
2022-09-23 15:00:21 +0500

    Changes Tasks

---
+++ Task1.py
@@ -0,0 +1,21 @@
+from functools import reduce
+
+field = range (1, 10)
+
+def draw_field(field):
+    print('-------------')
+    for i in range(3):
+        print('|', field[0+i*3], '|', field[1+i*3], '|', field[2+i*3], '|')
+        print('-------------')
+
+draw_field(field)
+
+
+
+
+from functools import reduce
+
+d = [1, 2, 3, 4, 5]
+
+pr = reduce(lambda x, y: x*y, d)
+print (pr)
